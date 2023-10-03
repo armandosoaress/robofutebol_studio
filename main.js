447cbd40ecb496fea837e6fd0b0561b4c765a61f
armandosoaress <linkssoares123@gmail.com>
2023-10-03 16:45:58 -0300

    up

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer');
 const { Telegraf, Markup } = require('telegraf');
 const fs = require('fs');
 
+
 var browser = null;
 
 async function main() {
@@ -11,7 +12,8 @@ async function main() {
         var red = 0;
 
         browser = await puppeteer.launch({
-            executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+            // "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+            executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe',
             headless: false
         });
 
@@ -91,7 +93,7 @@ async function main() {
         }
         function telegram(message) {
             const bot = new Telegraf("6565328483:AAGOUIwrwo3xiKrqKJsc40wGvDb6aWM67mc")
-            bot.telegram.sendMessage("-1001832440382", message, { parse_mode: 'HTML' })
+            bot.telegram.sendMessage("-1001807619335", message, { parse_mode: 'HTML' })
         }
 
         const getSocket = response => {
